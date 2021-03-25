3fb22514f75c43ac715d407df2da1fa03e8b9681
Kevin Sajan <kevsaj@gmail.com>
2021-03-24 22:41:06 -0400

    header updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+import Header from "./components/Header/Header.js"
+import BookSearch from "./components/BookSearch/BookSearch.js"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>What-up</h1>
+        <Header />
+        <BookSearch />
       </div>
     );
   }
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <header>
+        <h1 id="headerText">Find the Best Books dying to be read!</h1>
+        </header>
+    )
+}
+
+export default Header;
