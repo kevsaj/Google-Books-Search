93d064e7f2da63773032efcff1596d9ec5178ed4
Kevin Sajan <kevsaj@gmail.com>
2021-03-24 22:42:28 -0400

    book search component

---
+++ client/src/components/BookSearch/BookSearch.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import SearchBar from "../SearchBar/SearchBar.js";
+import BookList from "../BookList/BookList.js";
+import request from "superagent";
+import "./style.css"
+
+
+class BookSearch extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            books: [],
+            searchField: ""
+        }
+    }
+
+    searchBook = (event) => {
+        event.preventDefault();
+        request
+            .get("https://www.googleapis.com/books/v1/volumes")
+            .query({ q: this.state.searchField })
+            .then((data) => {
+                console.log(data);
+                this.setState({ books: [...data.body.items] })
+        })
+    }
+
+    render() {
+      return (
+        <div id="container">
+          <SearchBar searchBook={this.searchBook} handleSearch={this.handleSearch} />
+          <BookList books={this.state.books} />
+        </div>
+      );
+    }
+  } 
+  
+  export default BookSearch;
